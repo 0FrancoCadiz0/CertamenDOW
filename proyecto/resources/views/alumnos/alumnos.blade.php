971597ad7e85e95e41883ac57ab58385b4fe17c7
kevin alonso <vera.kvn.alonso@gmail.com>
2023-06-07 20:24:37 -0400

    modificacion alumnos y rellenar campos de bd alumnos

--- proyecto/resources/views/alumnos/alumnos.blade.php
+++ proyecto/resources/views/alumnos/alumnos.blade.php
@@ -7,66 +7,67 @@
     <title>Alumno</title>
     <link rel="stylesheet" href="css/custom.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <style>
+      body {
+        background-image: url('images/alumnos.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
 
+      .container {
+        margin-top: 50px;
+      }
+
+      table {
+        border-collapse: collapse;
+        width: 100%;
+        max-width: 500px;
+        margin: 0 auto;
+        background-color: ;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+
+      th, td {
+        border: 1px solid black;
+        padding: 10px;
+        text-align: center;
+      }
+
+      h3 {
+        text-align: center;
+        margin-top: 20px;
+      }
+    </style>
 </head>
-<body  style='background-image url(images/alumnos.jpg;); background-repeat:no-repeat; background-size:cover' >
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 col-lg-8">
-          <table class="table table-bordered table-striped table-hover">
-            <thead>
-              <tr>
-                <th>Estado de la entrega</th>
-                <th>Archivos enviados</th>
-                <th>Comentarios de la entrega</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>fsefefesf</td>
-                <td>sdfdsfsdfsdf</td>
-                <td>sdsdsdsd</td>
-              </tr>
-            </tbody>
+<body>
+    <div class="container py-3 px-3">
+      <div class="align py-5 px-3">
+        <h1>Taller de Sistemas</h1>
+        <hr> 
+        <h4>Alumnos</h4>
+      </div>  
+    </div>
+    <div class="row">
+      <h3>Entregas y Estados</h3>
+      <hr>
+      <div class="col-12 col-lg-3">
+          <table>
+            <tr>
+              <td><h5>Estado de la entrega</h5></td>
+              <td><h6>Sin enviar</h6></td>
+            </tr>
+            <tr>
+              <td><h5>Adjuntar Trabajo</h5></td>
+              <td><h6>Aquí subir el PDF</h6></td>
+            </tr>
+            <tr>
+              <td><h5>Estado</h5></td>
+              <td><h6>aprobado/rechazado/modificar</h6></td>
+            </tr>
+            
           </table>
-        </div>
-
       </div>
-      <h1 class="text-center mt-5">Subir Propuestas - Taller de Sistemas</h1>
-      <hr>
-
-      <h3>1. Ingresar Propuesta de Proyecto</h3>
-      <form>
-        <div class="form-group">
-          <label for="proyecto">Nombre del Proyecto</label>
-          <input type="text" class="form-control" id="proyecto" placeholder="Ingrese el nombre del proyecto">
-        </div>
-        <div class="form-group">
-          <label for="descripcion">Descripción del Proyecto</label>
-          <textarea class="form-control" id="descripcion" rows="3" placeholder="Ingrese la descripción del proyecto">
-</textarea>
-        </div>
-        <button type="submit" class="btnTuned">Subir Propuesta</button>
-      </form>
-
-      <hr>
-
-      <h3>2. Estado de Revisión de mi Propuesta</h3>
-      <div class="alert alert-info" role="alert">
-        Su propuesta está en revisión. Por favor, espere la respuesta del profesor.
-      </div>
-      <button class="btn btn-success">Iniciar Ejecución del Proyecto</button>
-
-      <hr>
-
-      <h3>3. Retroalimentación de los Profesores</h3>
-      <div class="alert alert-warning" role="alert">
-        Su propuesta requiere modificaciones. Por favor, realice los cambios necesarios.
-      </div>
-      <button class="btn btn-primary">Ver Retroalimentación</button>
     </div>
-    </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
